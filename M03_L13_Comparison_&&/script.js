c6f4a3cbee1f696726834d86fd3861c7fcf96cd3
Lotuslinny <67099000+Lotuslinny@users.noreply.github.com>
2020-09-03 14:19:49 +0200

    Add files via upload
    M03_L13_Logical_Operators

---
+++ M03_L13_Comparison_&&/script.js
@@ -0,0 +1,54 @@
+// Deel 1
+const age = 18;
+
+/* if (age > 17) {
+  console.log('you may enter');
+} else if (age < 18) {
+  console.log('please stay outside'); */
+
+//Operators Deel 1
+if (age > 17 && age < 26) {
+  console.log('you get 50 % off');
+} else if (age < 18 || age > 25);
+console.log(" Pay your drinks")
+
+// Deel 2
+const isFemale = 'male';
+
+if (isFemale == "female") {
+  console.log('true');
+} else if (isFemale == "male") {
+  console.log('false');
+}
+
+
+// Operators Deel 2
+const name = 'Lin Ny';
+
+if (name == 'Bram' || name == 'Abraham') {
+  console.log('One free beer for you!');
+} else if (name !== 'Bram' || name !== 'Abraham')
+  console.log(' Go on buy a drink!')
+
+
+
+// Deel 3
+
+const driverStatus = 'Jason';
+
+if (driverStatus == "Bob") {
+  console.log("You can drive");
+} else if (driverStatus == "Jason") {
+  console.log("You may not drive");
+}
+
+// Operators Deel 3
+
+const totalAmount = '100';
+if (totalAmount >= 25 && totalAmount < 50) {
+  console.log(' You get a free portion of bitterballs!')
+} else if (totalAmount >= 50 && totalAmount < 100) {
+  console.log('You get a free portion of nachos!');
+} else if (totalAmount >= 100) {
+  console.log('You will get a free botlle of champagne!')
+}
